5ec7b578b680a1b2edac1df09195cd15f2e46f74
Mezturbate <mezencarloslababidi@gmail.com>
2025-08-01 06:23:23 +0800

    connected to firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 android {
@@ -59,5 +60,7 @@ dependencies {
         androidTestImplementation(libs.androidx.ui.test.junit4)
         debugImplementation(libs.androidx.ui.tooling)
         debugImplementation(libs.androidx.ui.test.manifest)
+        implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+        implementation("com.google.firebase:firebase-analytics")
     }
 }
